4060b46a0d6a37e6c11a160a9f21131c5b332108
b3n3b0 <jags4real25@gmail.com>
2018-08-02 13:48:16 +0100

    Testing
    Testing the mock api out "

--- tut/quickstart/views.py
+++ tut/quickstart/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrb.auth.models import User, Group
+from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from tut.quickstart.serializers import UserSerializer, GroupSerializer
 
